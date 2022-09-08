2a70f7b95e5ba6cabb1eeb3a408ba9ad58c563e4
Lucas Krantz <lucas.krantz@elev.ga.ntig.se>
2022-09-08 14:18:10 +0200

    hfjkhgfghjk

---
+++ INTERACTIVESTORY/Program.cs
@@ -0,0 +1,49 @@
+﻿
+
+Console.WriteLine("Det är ett stort berg framför dig. Bestiger du berget? Svara Ja eller Nej");
+
+string val = Console.ReadLine();
+
+
+
+if (val == "Nej")
+{
+    Console.WriteLine("Bra val. Tryck på enter för att avsluta.");
+
+
+}
+
+else if (val == "Ja")
+{
+
+    Console.WriteLine("Pfft, eru dum i hubvet. Skit i. Du klättrar upp men stannar vid en ravin. Vågar du klättra över?");
+    val = Console.ReadLine();
+
+
+    if (val == "Nej")
+    {
+
+        Console.WriteLine("Försent. Marken under dig kollapsar och du dör.");
+        Console.WriteLine("GAME OVER");
+        Console.WriteLine("Tryck på ENTER för att avsluta");
+
+
+    }
+
+
+
+    else if (val == "Ja")
+    {
+
+
+        Console.WriteLine("Whatever. Du klarade dig knappt över bara för att du har tur.");
+        Console.WriteLine("");
+    }
+
+
+}
+
+
+
+
+Console.ReadLine();
